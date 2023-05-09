3053bfbf047e38a00db5cd6eb3c0a62f6209fca7
Brian L <bclipp770@gmail.com>
2023-05-09 12:38:00 -0400

    first attempt cluster

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 terraform {
   cloud {
-    organization = "chapter_8"
+    organization = "brians_stuff"
 
     workspaces {
       name = "cli"
@@ -30,19 +30,30 @@ provider "databricks" {
 }
 
 
-data "databricks_spark_version" "latest_lts" {
+data "databricks_spark_version" "ETL" {
+  spark_version = "1"
+  cluster_name = "tiny-etl"
   long_term_support = true
 }
 
-resource "databricks_cluster" "tiny" {
+data "databricks_spark_version" "ML" {
+  long_term_support = true
+  spark_version = "13.0 ML"
+  cluster_name = "tiny-ml"
+}
+
+resource "databricks_cluster" "tiny-packt" {
   cluster_name = "tiny"
-  spark_version = data.databricks_spark_version.latest_lts.id
-  node_type_id = "m4.large"
-  driver_node_type_id = "m4.large"
+  spark_version = data.databricks_spark_version.etl.id
+  node_type_id = "m5.large"
   autotermination_minutes = 10
   autoscale {
     min_workers = 1
     max_workers = 2
+  }
+   spark_conf = {
+    "spark.databricks.cluster.profile" : "singleNode"
+    "spark.master" : "local[*]"
   }
   aws_attributes {
     first_on_demand = 1
@@ -54,3 +65,28 @@ resource "databricks_cluster" "tiny" {
     ebs_volume_size = 100
   }
 }
+
+resource "databricks_cluster" "tiny-packt-ml" {
+  cluster_name = "tiny"
+  spark_version = data.databricks_spark_version.databricks_spark_version.id
+  node_type_id = "g4dn.xlarge"
+  autotermination_minutes = 10
+  autoscale {
+    min_workers = 1
+    max_workers = 2
+  }
+     spark_conf = {
+    "spark.databricks.cluster.profile" : "singleNode"
+    "spark.master" : "local[*]"
+  }
+  aws_attributes {
+    first_on_demand = 1
+    availability = "SPOT_WITH_FALLBACK"
+    zone_id = "us-west-2b"
+    spot_bid_price_percent = 100
+    ebs_volume_type = "GENERAL_PURPOSE_SSD"
+    ebs_volume_count = 3
+    ebs_volume_size = 100
+  }
+}
+

aa439786bf6c2f24827780aa8b45b121bb8977b7
Brian L <bclipp770@gmail.com>
2023-05-12 11:35:25 -0400

    Bclipp add terraform (#1)
    added tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 
 terraform {
+  required_version = ">= 1.3.9"
   cloud {
     organization = "brians_stuff"
 
@@ -7,9 +8,9 @@ terraform {
       name = "cli"
     }
   }
-required_providers {
+  required_providers {
     databricks = {
-      source = "databricks/databricks"
+      source  = "databricks/databricks"
       version = "1.0.0"
     }
   }
@@ -25,7 +26,7 @@ variable "TOKEN" {
 
 
 provider "databricks" {
-  host = var.HOST
+  host  = var.HOST
   token = var.TOKEN
 }
 
@@ -35,119 +36,170 @@ data "databricks_spark_version" "latest_lts" {
 }
 
 data "databricks_spark_version" "gpu_ml" {
-  ml  = true
+  ml = true
+}
+
+resource "databricks_library" "jobs_cluster_lib_etl" {
+  cluster_id = databricks_cluster.tiny-packt.id
+  pypi {
+    package = "etl-jobs==0.1.1"
+  }
+}
+
+resource "databricks_library" "jobs_cluster_lib_schema" {
+  cluster_id = databricks_cluster.tiny-packt.id
+  pypi {
+    package = "schema-jobs==0.1.0"
+  }
+}
+
+resource "databricks_library" "jobs_cluster_lib_ml" {
+  cluster_id = databricks_cluster.tiny-packt-ml.id
+  pypi {
+    package = "ml-jobs==0.1.0"
+  }
 }
 
 
-# https://www.databricks.com/blog/2022/12/5/databricks-workflows-through-terraform.html
-# https://stackoverflow.com/questions/75579462/how-to-create-azure-databricks-jobs-of-type-python-wheel-by-terraform
-#workflow 1: ML
-#        1. create model
-#          wheel pip install ml-jobs
+resource "databricks_cluster" "tiny-packt" {
+  cluster_name            = "tiny-packt-etl"
+  spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = "m5.large"
+  autotermination_minutes = 10
+  autoscale {
+    min_workers = 1
+    max_workers = 2
+  }
 
-#workflow 2: SCHEMA
-#        1. deploy schema
-#          wheel pip install schema-jobs
+  aws_attributes {
+    first_on_demand        = 1
+    availability           = "SPOT_WITH_FALLBACK"
+    zone_id                = "us-west-2b"
+    spot_bid_price_percent = 100
+    ebs_volume_type        = "GENERAL_PURPOSE_SSD"
+    ebs_volume_count       = 3
+    ebs_volume_size        = 100
+  }
+}
 
-#workflow 3: ETL
-#        1. get data
-#          wheel ???? missing!!!!
-#        2. ETL data
-#          wheel pip install etl-jobs
-
-#workflow 1: ETL Int Tests
-#        1. run int tests
-#          wheel pip install etl-jobs
+resource "databricks_cluster" "tiny-packt-ml" {
+  cluster_name            = "tiny-packt-ml"
+  node_type_id            = "g4dn.xlarge"
+  autotermination_minutes = 10
+  spark_version           = data.databricks_spark_version.gpu_ml.id
+  autoscale {
+    min_workers = 1
+    max_workers = 2
+  }
+  aws_attributes {
+    first_on_demand        = 1
+    availability           = "SPOT_WITH_FALLBACK"
+    zone_id                = "us-west-2b"
+    spot_bid_price_percent = 100
+    ebs_volume_type        = "GENERAL_PURPOSE_SSD"
+    ebs_volume_count       = 3
+    ebs_volume_size        = 100
+  }
+}
 
 
 resource "databricks_job" "etl" {
- name = "Job with multiple tasks"
- max_concurrent_runs = 1
+  name                = "etl"
+  max_concurrent_runs = 1
 
- # job schedule
- #schedule {
- #  quartz_cron_expression = "0 0 0 ? 1/1 * *" # cron schedule of job
- #  timezone_id = "UTC"
- # }
-
- # notifications at job level
- email_notifications {
-   on_success = ["bclipp770@gmail.com", "bclipp770@gmail.com"]
-     on_start   = ["bclipp770@gmail.com"]
-     on_failure = ["bclipp770@gmail.com"]
- }
-
-   job_cluster {
-   new_cluster {
-
-    spark_version = data.databricks_spark_version.latest_lts.id
-    node_type_id = "m5.large"
-     #spark_env_vars = {
-     #  PYSPARK_PYTHON = "/databricks/python3/bin/python3"
-     #}
-     num_workers        = 1
-     data_security_mode = "NONE"
-  spark_version = data.databricks_spark_version.latest_lts.id
-  node_type_id = "m5.large"
-  autotermination_minutes = 10
-
-  aws_attributes {
-    first_on_demand = 1
-    availability = "SPOT_WITH_FALLBACK"
-    zone_id = "us-west-2b"
-    spot_bid_price_percent = 100
-    ebs_volume_type = "GENERAL_PURPOSE_SSD"
-    ebs_volume_count = 3
-    ebs_volume_size = 100
-  }
-   }
-   job_cluster_key = "Shared_job_cluster"
- }
-
-  task {
-    task_key = "a"
-    name = "Extract"
-    library {
-     pypi {
-       package = "faker"
-     }
-   }
-        python_wheel_task {
-      package_name = "etl-jobs"
-      entry_point = "main"
-    }
-
-       # timeout and retries
-   timeout_seconds = 1000
-   min_retry_interval_millis = 900000
-   max_retries = 1
+  email_notifications {
+    on_success = ["bclipp770@gmail.com", "bclipp770@gmail.com"]
+    on_start   = ["bclipp770@gmail.com"]
+    on_failure = ["bclipp770@gmail.com"]
   }
 
-
-
   task {
-    name = "Transform and Load"
-    task_key = "b"
-
-   # you can stack multiple depends_on blocks
-   depends_on {
-     task_key = "name_of_my_first_task"
-   }
-
-   # libraries needed
-   library {
-     pypi {
-       package = "faker"
-     }
-   }
-        python_wheel_task {
-      package_name = "etl-jobs"
-      entry_point = "main"
+    task_key            = "a_extract"
+    existing_cluster_id = databricks_cluster.tiny-packt.id
+    python_wheel_task {
+      package_name = "etl_jobs"
+      entry_point  = "main"
     }
 
-   # timeout and retries
-   timeout_seconds = 1000
-   min_retry_interval_millis = 900000
-   max_retries = 1
- }
+    timeout_seconds           = 1000
+    min_retry_interval_millis = 900000
+    max_retries               = 1
+  }
+
+  task {
+    task_key            = "b_ransform_and_Load"
+    existing_cluster_id = databricks_cluster.tiny-packt.id
+    depends_on {
+      task_key = "a_extract"
+    }
+
+    python_wheel_task {
+      package_name = "etl_jobs"
+      entry_point  = "main"
+    }
+
+    timeout_seconds           = 1000
+    min_retry_interval_millis = 900000
+    max_retries               = 1
+  }
+}
+
+resource "databricks_job" "schema-jobs" {
+  name                = "schema-jobs"
+  max_concurrent_runs = 1
+
+  # job schedule
+  #schedule {
+  #  quartz_cron_expression = "0 0 0 ? 1/1 * *" # cron schedule of job
+  #  timezone_id = "UTC"
+  # }
+
+
+  email_notifications {
+    on_success = ["bclipp770@gmail.com", "bclipp770@gmail.com"]
+    on_start   = ["bclipp770@gmail.com"]
+    on_failure = ["bclipp770@gmail.com"]
+  }
+
+  task {
+    task_key            = "schema-jobs"
+    existing_cluster_id = databricks_cluster.tiny-packt.id
+    python_wheel_task {
+      package_name = "schema_jobs"
+      entry_point  = "main"
+    }
+    timeout_seconds           = 1000
+    min_retry_interval_millis = 900000
+    max_retries               = 1
+  }
+}
+
+resource "databricks_job" "ml-jobs" {
+  name                = "ml-jobs"
+  max_concurrent_runs = 1
+
+  # job schedule
+  #schedule {
+  #  quartz_cron_expression = "0 0 0 ? 1/1 * *" # cron schedule of job
+  #  timezone_id = "UTC"
+  # }
+
+
+  email_notifications {
+    on_success = ["bclipp770@gmail.com", "bclipp770@gmail.com"]
+    on_start   = ["bclipp770@gmail.com"]
+    on_failure = ["bclipp770@gmail.com"]
+  }
+
+  task {
+    task_key            = "ml-jobs"
+    existing_cluster_id = databricks_cluster.tiny-packt.id
+    python_wheel_task {
+      package_name = "ml_jobs"
+      entry_point  = "main"
+    }
+    timeout_seconds           = 1000
+    min_retry_interval_millis = 900000
+    max_retries               = 1
+  }
 }
